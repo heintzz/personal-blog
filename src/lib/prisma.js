d0548ba0e628116ccf84b5c59b069d64b7db22b9
heintzz <mhasnanr@gmail.com>
2025-11-20 04:43:30 +0700

    fix: remove prisma accelerate since the cloudflare is down

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,9 +1,11 @@
-import { PrismaClient } from '../app/generated/prisma';
-import { withAccelerate } from '@prisma/extension-accelerate';
+// src/lib/prisma.js
+import { PrismaClient } from '@prisma/client';
 
 const globalForPrisma = global;
 
-const prisma = globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate());
+// Cache the Prisma Client in a global variable to avoid creating new connections on every request,
+// which is especially important in a serverless environment.
+const prisma = globalForPrisma.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
 
