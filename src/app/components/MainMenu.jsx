365101c153435e90a1cf7a5cd41d8b40bf85e5da
heintzz <mhasnanr@gmail.com>
2025-11-20 04:51:24 +0700

    refactor: layout structure

--- src/app/components/MainMenu.jsx
+++ src/app/components/MainMenu.jsx
@@ -1,17 +1,6 @@
 'use client';
 
-import { useState } from 'react';
-import TopBar from './TopBar';
 
 export default function MainMenu({ children }) {
-  const [sidebarOpen, setSidebarOpen] = useState(true);
-  return (
-    <div className="flex-1 flex flex-col overflow-hidden">
-      {/* Top Bar */}
-      <TopBar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
-
-      {/* Main Content */}
-      {children}
-    </div>
-  );
+  return <div className="flex-1 flex flex-col overflow-hidden">{children}</div>;
 }
